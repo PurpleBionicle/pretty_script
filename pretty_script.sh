0ff4ca08531fe8e3ec7ad2cf26dfcfa6f7ec757d
88888812 <ndp2012@yandex.ru>
2023-01-02 18:39:52 +0300

    changed sh

--- pretty_script.sh
+++ pretty_script.sh
@@ -1,6 +1,17 @@
 sudo apt update
 sudo apt-get install libsox-fmt-all
 sudo apt install sox
+sudo apt-get install python3-tk
 play 1.mp3 &
 python3 heart.py &
 wait
+
+
+if [ -n "$1" ]
+then
+sudo apt-get remove sox
+sudo apt-get remove libsox-fmt-all && sudo apt-get autoremove
+else
+echo "Удаление не произведено "
+fi
+
